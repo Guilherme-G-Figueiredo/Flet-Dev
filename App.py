c15fb451a4187de0698c6a1bb6fdd1a9a2b0af88
Guilherme Gambetta Figueiredo <guilherme.gambetta@hotmail.com>
2024-10-13 14:25:38 -0300

    Update App.py

--- App.py
+++ App.py
@@ -16,8 +16,8 @@ def main(pagina):
         campo_mensagem.value = ""
         pagina.update()
 
-    campo_mensagem = ft.TextField(label="Escreva sua mensagem", on_submit=enviar_mensagem, autofocus=True)
-    botao_enviar = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+    campo_mensagem = ft.TextField(label="Write your mensage", on_submit=enviar_mensagem, autofocus=True)
+    botao_enviar = ft.ElevatedButton("Send", on_click=enviar_mensagem)
     linha_enviar = ft.Row([campo_mensagem, botao_enviar])
     
     def entrar_chat(evento):
@@ -25,13 +25,13 @@ def main(pagina):
         pagina.remove(texto)
         pagina.remove(botao_iniciar)
         pagina.add(chat)
-        pagina.pubsub.send_all(f"{nome_usuario.value} entrou no chat")
+        pagina.pubsub.send_all(f"{nome_usuario.value} joined the chat")
         pagina.add(linha_enviar)
         pagina.update()
     
-    titulo_popup = ft.Text("Bem-vindo ao PyChat!")
-    nome_usuario = ft.TextField(label="Escreva seu nome aqui", on_submit=entrar_chat, autofocus=True)
-    botao_popup = ft.ElevatedButton("Entrar Chat", on_click=entrar_chat)
+    titulo_popup = ft.Text("Welcome to PyChat!")
+    nome_usuario = ft.TextField(label="Write your name here", on_submit=entrar_chat, autofocus=True)
+    botao_popup = ft.ElevatedButton("Enter Chat", on_click=entrar_chat)
     popup = ft.AlertDialog(
         open=False,
         modal=True,
@@ -45,7 +45,7 @@ def main(pagina):
         popup.open = True
         pagina.update()
 
-    botao_iniciar = ft.ElevatedButton("Iniciar Chat", on_click=abrir_popup)
+    botao_iniciar = ft.ElevatedButton("Start Chat", on_click=abrir_popup)
 
     pagina.add(texto)
     pagina.add(botao_iniciar)
